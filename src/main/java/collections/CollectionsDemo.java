7ce9607946904a1b5aabdd70e71f5b3512372b92
Vladimir Dubovsky <duuubbb@gmail.com>
2021-11-20 21:28:02 +0300

    Initial commit

---
+++ src/main/java/collections/CollectionsDemo.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class CollectionsDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Collection.class);
+
+        System.out.println(List.class);
+        System.out.println(Set.class);
+        System.out.println(Queue.class);
+    }
+}
